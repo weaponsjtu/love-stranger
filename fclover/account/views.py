76eac99287507f12cb0f5cb3989319574ad4ad5f
Ye Pan <panye.sjtu@gmail.com>
2014-02-21 15:49:35 +0400

    modify again

--- fclover/account/views.py
+++ fclover/account/views.py
@@ -5,9 +5,20 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User
 from django.template import RequestContext
+from fclover.account.forms import SigninForm
+from fclover.account.models import UserProfile
+from django.contrib.auth import authenticate
 
 def signup(request):
     return render_to_response('signup.html', context_instance=RequestContext(request))
 
 def signin(request):
-    return render_to_response('signin.html', context_instance=RequestContext(request))
+    if request.method == 'POST':
+        form = SigninForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            return HttpResponse('Welcome back!')
+    else:
+        form = SigninForm()
+    return render_to_response('signin.html', {'form': form},context_instance=RequestContext(request))
+
