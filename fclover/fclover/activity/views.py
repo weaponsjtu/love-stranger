8797d4c4adc82bf31a59d9781c3e203c40294f00
weapon <javawebzwp@gmail.com>
2014-03-03 18:03:47 +0400

    activity create

--- fclover/fclover/activity/views.py
+++ fclover/fclover/activity/views.py
@@ -13,7 +13,30 @@ def index(request):
     return render_to_response('index.html', {'activity_list': activity_list}, context_instance=RequestContext(request))
 
 def create(request):
-    return render_to_response('activity/create.html', context_instance=RequestContext(request))
+    if request.POST.__len__() == 0:
+        return render_to_response('activity/create.html', context_instance=RequestContext(request))
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        picture = request.POST['picture']
+        createTime = request.POST['createTime']
+        dueTime = request.POST['dueTime']
+        beginTime = request.POST['beginTime']
+        endTime = request.POST['endTime']
+        location = request.POST['location']
+        maxMale = request.POST['maxMale']
+        maxFemale = request.POST['maxFemale']
+        type = request.POST['type']
+        user_id = reqeust.POST['user_id']
+        act = Activity(title = title, picture = picture, content = content, createTime = createTime, dueTime = dueTime, beginTime = beginTime, endTime = endTime, location = location, maxMale = maxMale, maxFemale = maxFemale, type = type, initiator_id = user_id)
+        # TODO check all parameters
+        if check_parameter(act):
+            act.save()
+            # TODO save activity object
+            return HttpResponseRedirect(reverse('fclover.activity.views.index', None))
+        else:
+            return render_to_response('activity/create.html', {"message": "parameter wrong"}, context_instance=RequestContext(request))
+
 
 def delete(request):
     return
